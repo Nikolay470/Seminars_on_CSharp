82224dd9cba33c5ebb5258cd5dc5750480e49801
Nikolay470 <dyachkovnick@yandex.ru>
2023-04-21 22:23:09 +0300

    Task05

---
+++ Task05/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите целое число.");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 0)
+{
+    for(int count = -number; count <= number; count++) 
+    {
+        Console.Write(count + " ");
+    }
+} 
+else 
+{
+    for(int count = number; count <= -number; count++) 
+    {
+        Console.Write(count + " ");
+    }
+}
