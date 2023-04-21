95074f6b4eb5076a1a2bda9382f134e524e92ecf
Nikolay470 <dyachkovnick@yandex.ru>
2023-04-21 22:33:05 +0300

    Task07

---
+++ Task07/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите трехзначное целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 0) number *= -1;
+if (number > 99 && number < 1000)
+{
+    int result = number % 10;
+    Console.WriteLine(result);
+}
+else Console.WriteLine("Введите коректное целое число");
+
