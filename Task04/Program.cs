e5c1911a80557f30c50e968e5019067062534538
Nikolay470 <dyachkovnick@yandex.ru>
2023-04-22 08:14:51 +0300

    Task04

---
+++ Task04/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход три числа и
+// выдает максимальное из этих чисел.
+
+Console.WriteLine("Введите первое целое число.");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе целое число.");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье целое число.");
+int thirdNumber = Convert.ToInt32(Console.ReadLine());
+
+int max = firstNumber;
+
+if (secondNumber > max) max = secondNumber;
+if (thirdNumber > max) max = thirdNumber; 
+
+Console.WriteLine("Максимальное число " + max);
