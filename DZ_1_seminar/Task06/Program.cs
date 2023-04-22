7176ff549290480eb9585d3f96a3f21a18ede059
Nikolay470 <dyachkovnick@yandex.ru>
2023-04-22 08:25:41 +0300

    Task06

---
+++ DZ_1_seminar/Task06/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая принимает на вход целое число и
+// выдает является ли оно четным или нет.
+
+Console.WriteLine("Введите целое число.");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0) Console.WriteLine($"Число {number} четное");
+else Console.WriteLine($"Число {number} не является четным");
