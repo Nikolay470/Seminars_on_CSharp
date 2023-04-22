8cdc1e0bb186bc335c83cee2fcf976c690fc2778
Nikolay470 <dyachkovnick@yandex.ru>
2023-04-22 07:51:54 +0300

    Task02

---
+++ Task02/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите первое число.");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число.");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if (firstNumber > secondNumber) Console.WriteLine("Наибольшее число " + firstNumber);
+else Console.WriteLine("Наибольшее число " + secondNumber);
