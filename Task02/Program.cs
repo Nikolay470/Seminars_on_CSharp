d6d1fb343ab141acabf21f825db5b60dd9a74212
Nikolay470 <dyachkovnick@yandex.ru>
2023-04-22 08:02:25 +0300

    Changed a file Task02/Program.cs

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -1,7 +1,18 @@
-﻿Console.WriteLine("Введите первое число.");
+﻿// Напишите программу, которая на вход принимает два числа и выдает
+// какое больше, а какое меньше.
+
+Console.WriteLine("Введите первое число.");
 int firstNumber = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число.");
 int secondNumber = Convert.ToInt32(Console.ReadLine());
 
-if (firstNumber > secondNumber) Console.WriteLine("Наибольшее число " + firstNumber);
-else Console.WriteLine("Наибольшее число " + secondNumber);
+if (firstNumber > secondNumber) 
+{
+Console.WriteLine("Большее число " + firstNumber);
+Console.WriteLine("Меньшее число " + secondNumber);
+}
+else 
+{
+Console.WriteLine("Большее число " + secondNumber);
+Console.WriteLine("Меньшее число " + firstNumber);
+}
