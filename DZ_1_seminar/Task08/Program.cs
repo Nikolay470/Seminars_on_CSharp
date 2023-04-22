1f31e8976098df8fa8a24eb7c0efb6fbdd634db3
Nikolay470 <dyachkovnick@yandex.ru>
2023-04-22 08:58:53 +0300

    Task08

---
+++ DZ_1_seminar/Task08/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход число N, а на выходе
+// выдает все четные числа от 1 до N.
+
+Console.WriteLine("Введите целое положительное число.");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number <= 0) Console.WriteLine("Введите корректное число.");
+
+for (int count = 2; count <= number; count += 2)
+{
+    Console.Write(count + " ");
+}
