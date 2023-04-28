39dd22188ce123bb9c1f7d6b7e6f523720aa4827
Nikolay470 <dyachkovnick@yandex.ru>
2023-04-28 19:08:51 +0300

    Added a folder DZ_2_seminar/task10

---
+++ DZ_2_seminar/Task10/Program.cs
@@ -0,0 +1,20 @@
+﻿int SearchSecondDigit(int num)
+{
+    int result = (num / 10) % 10;
+    if(result < 0)
+    {
+        result *= -1;
+    }
+    return result;
+}
+
+Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number > 99 && number < 1000 || number < -99 && number > -1000)
+{
+    int secondDigit = SearchSecondDigit(number);
+    Console.WriteLine($"Вторая цифра числа {number} это {secondDigit}");
+}
+else Console.WriteLine("Введите коректное число");
+
