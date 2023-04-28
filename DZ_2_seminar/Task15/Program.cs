f9f23d426159095f4c8340b15087ee23e56ebb3d
Nikolay470 <dyachkovnick@yandex.ru>
2023-04-28 20:01:23 +0300

    Added a folder DZ_2_seminar/Task15

---
+++ DZ_2_seminar/Task15/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+bool DefineDayOff(int num)
+{
+    if (num == 6 || num == 7) return true;
+    else return false;
+}
+
+Console.WriteLine("Введите номер дня недели.");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 1 || number > 7) Console.WriteLine("Введите коректный номер дня недели.");
+else
+{
+    bool result = DefineDayOff(number);
+    Console.WriteLine(result ? "Этот день выходной" : "Этот день будний");
+}
+
