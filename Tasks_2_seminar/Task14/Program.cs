f48bbb31072dad6e81aa92bac09c2adfb0a78a15
Nikolay470 <dyachkovnick@yandex.ru>
2023-04-28 15:51:59 +0300

    Added a folder Tasks_2_seminar/Task14

---
+++ Tasks_2_seminar/Task14/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу которая принимает на вход число и проверяет является ли
+// оно кратным одновременно 7 и 23
+
+bool CompareMultiplicity(int num)
+{
+    return num % 7 == 0 && num % 23 == 0;
+}
+
+Console.WriteLine("Введите ваше число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool result = CompareMultiplicity(number);
+
+Console.WriteLine(result ? "да" : "нет");
