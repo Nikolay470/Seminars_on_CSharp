2fa2294b2c2962ab442f93737d39e398ce95c0b9
Nikolay470 <dyachkovnick@yandex.ru>
2023-04-28 14:34:51 +0300

    Added a folder Tasks_2_seminar/Task09

---
+++ Tasks_2_seminar/Task09/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу которая выводит число из отрезка [10, 99]
+// и показывает наибольшую цифру числа.
+
+int number = new Random().Next(10, 100);
+Console.WriteLine($"Случайное число это {number}");
+
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+
+int result = firstDigit > secondDigit ? firstDigit : secondDigit;
+Console.WriteLine($"Наибольшая цифра числа {number} это {result}");
