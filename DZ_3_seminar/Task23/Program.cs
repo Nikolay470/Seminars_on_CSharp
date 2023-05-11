2de8bae3cc49229275e8e86f68bed1d307e718fa
Nikolay470 <dyachkovnick@yandex.ru>
2023-05-11 20:02:11 +0300

    Added a folder DZ_3_seminar/Task23

---
+++ DZ_3_seminar/Task23/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+void Cube(int num)
+{
+    if (num > 0)
+    {
+        for (int count = 1; count <= num; count++)
+        {
+            double cube = Math.Pow(count, 3);
+            Console.WriteLine($"{count,4} => {cube,7}");
+        }
+    }
+    else
+    {
+        for (int count = -1; count >= num; count--)
+        {
+            double cube = Math.Pow(count, 3);
+            Console.WriteLine($"{count,4} => {cube,7}");
+        }
+    }
+}
+
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number == 0) Console.WriteLine("Введите число отличное от нуля");
+else Cube(number);
