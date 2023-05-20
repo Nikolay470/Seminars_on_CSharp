0a73f8f3efe8fd527689c3449d5f34e1b4c66ce7
Nikolay470 <dyachkovnick@yandex.ru>
2023-05-20 19:30:43 +0300

    Added a folder DZ_4_seminar/Task25

---
+++ DZ_4_seminar/Task25/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+
+int Exponentiate(int num, int deg)
+{
+    int sum = num;
+    for (int n = 1; n < deg; n++)
+    {
+        sum = sum * num;
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень в которую надо возвести число");
+int degree = Convert.ToInt32(Console.ReadLine());
+if (degree <= 0) Console.WriteLine("Введите степень в виде целого положительного числа");
+else
+{
+    int result = Exponentiate(number, degree);
+    Console.WriteLine($"{number} в степени {degree} равно {result}");
+}
