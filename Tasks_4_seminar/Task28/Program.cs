881001092a6959ca97155b0c635a5b26f3e7fe51
Nikolay470 <dyachkovnick@yandex.ru>
2023-05-20 17:30:07 +0300

    Added a folder Tasks_4_seminar/Task28

---
+++ Tasks_4_seminar/Task28/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу которая принимает на вход число N 
+// и выдает произведение чисел от 1 до N 
+
+int Factorial(int num)
+{
+    if(num == 1) return 1;
+    else 
+    {
+        checked
+        {
+            return num * Factorial(num - 1);
+        }
+    }
+}
+
+Console.WriteLine("Введите целое положительное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number < 1) Console.WriteLine("Вы ввели некоректное число");
+else
+{
+int compositionNum = Factorial(number);
+Console.WriteLine($"Произведение чисел от 1 до {number} равно {compositionNum}");
+}
