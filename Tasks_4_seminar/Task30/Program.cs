ab86bbd3bf940f910a5bf84896f019fcdc399bb2
Nikolay470 <dyachkovnick@yandex.ru>
2023-05-20 18:15:23 +0300

    Added a folder Tasks_4_seminar/Task30

---
+++ Tasks_4_seminar/Task30/Program.cs
@@ -0,0 +1,39 @@
+﻿// Напишите программу которая выводит массив из 8 элементов,
+// заполненный нулями и единицами в случайном порядке
+
+void FillArray(int[] arr)
+{
+    var random = new Random();
+    for (int n = 0; n < arr.Length; n++)
+    {
+        arr[n] = random.Next(0, 2);
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    for (int n = 0; n < arr.Length; n++)
+    {
+        if (n < arr.Length - 1) Console.Write($"{arr[n]}, ");
+        else Console.Write($"{arr[n]}");
+    }
+}
+
+int[] CreateArray(int leng)
+{
+    int[] arr = new int[leng];
+    return arr;
+}
+
+Console.WriteLine("Введите колличество элементов в массиве (числом)");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 1) Console.WriteLine("Введите целое положительное число");
+else
+{
+    int[] array = CreateArray(number);
+    FillArray(array);
+
+    Console.Write("[");
+    PrintArray(array);
+    Console.Write("]");
+}
