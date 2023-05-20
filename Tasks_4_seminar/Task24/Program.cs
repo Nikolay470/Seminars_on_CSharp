4c9ec7f0a19eda0ddd19ee2fcb7f76ce7e3a9c0c
Nikolay470 <dyachkovnick@yandex.ru>
2023-05-20 16:29:40 +0300

    Added a folder Tasks_4_seminar/Task24

---
+++ Tasks_4_seminar/Task24/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу которая принимает на вход число А
+// и выдает сумму чисел от 1 до А
+
+int sumNumbers(int num)
+{
+    int sum = 0;
+    for(int i = 1; i <= num; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите целое положительное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number < 0) Console.WriteLine("Введите положительное число");
+else
+{
+    int sum = sumNumbers(number);
+    Console.WriteLine($"Сумма чисел от 1 до {number} равна {sum}");
+}
