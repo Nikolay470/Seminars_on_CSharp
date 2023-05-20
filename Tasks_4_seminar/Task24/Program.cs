ea3fd004c942ef813c86ecc63736fca4323b8d48
Nikolay470 <dyachkovnick@yandex.ru>
2023-05-20 16:57:14 +0300

    Added a folder Tasks_4_seminar/Task26

--- Tasks_4_seminar/Task24/Program.cs
+++ Tasks_4_seminar/Task24/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Напишите программу которая принимает на вход число А
 // и выдает сумму чисел от 1 до А
 
-int sumNumbers(int num)
+int SumNumbers(int num)
 {
     int sum = 0;
     for(int i = 1; i <= num; i++)
@@ -17,6 +17,6 @@ int number = Convert.ToInt32(Console.ReadLine());
 if(number < 0) Console.WriteLine("Введите положительное число");
 else
 {
-    int sum = sumNumbers(number);
+    int sum = SumNumbers(number);
     Console.WriteLine($"Сумма чисел от 1 до {number} равна {sum}");
 }
---
+++ Tasks_4_seminar/Task26/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу которая принимает на вход число
+// и выводит колличество цифр в числе
+
+
+int QuantityNumbers(int num)
+{
+    int count = 0;
+    while (num != 0)
+    {
+        num /= 10;
+        count++;
+    }
+    return count;
+}
+
+Console.WriteLine("Введите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number == 0) Console.WriteLine($"{number} => 1");
+else
+{
+    int quantityNum = QuantityNumbers(number);
+    Console.WriteLine($"{number} => {quantityNum}");
+}
+
