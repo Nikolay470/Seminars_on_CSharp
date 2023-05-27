cb2c81b01fcd891f0b26025c94bf49809fa8c579
Nikolay470 <dyachkovnick@yandex.ru>
2023-05-27 10:23:16 +0300

    Added a folder Tasks_5_seminar/task32

---
+++ Tasks_5_seminar/Task32/Program.cs
@@ -0,0 +1,45 @@
+﻿// 32. Напишите программу замены элементов массива.
+//     Положительные элементы заменить на соответствующие отрицательные
+//     и наооборот.
+int[] CreateArray(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void ReplaceElementsToOpposite(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] *= -1;        
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length - 1)
+                Console.Write($"{arr[i]}, ");
+        else       
+                Console.Write($"{arr[i]}");
+    }
+}
+
+int[] arrayTest = CreateArray(7, -10, 10);
+
+Console.Write("[");
+PrintArray(arrayTest);
+Console.WriteLine("]");
+
+ReplaceElementsToOpposite(arrayTest);
+
+Console.Write("[");
+PrintArray(arrayTest);
+Console.WriteLine("]");
