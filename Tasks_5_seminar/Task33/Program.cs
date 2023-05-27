7ce9c4c9f95169944dac102a3aabb06f62f46e53
Nikolay470 <dyachkovnick@yandex.ru>
2023-05-27 11:17:07 +0300

    Added a folder Tasks_5_seminar/Task33

---
+++ Tasks_5_seminar/Task33/Program.cs
@@ -0,0 +1,54 @@
+﻿// 33. Задайте массив.
+//     Напишите программу которая проверяет присутствует ли заданное число в массиве.
+
+int[] CreateArray(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+bool SearchNumberInArray(int[] arr, int num)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] == num)
+                return true;
+    }
+    return false; 
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length - 1)
+                Console.Write($"{arr[i]}, ");
+        else       
+                Console.Write($"{arr[i]}");
+    }
+}
+
+int GetInput(string message)
+{
+    Console.WriteLine(message);
+    int input = Convert.ToInt32(Console.ReadLine());
+    return input;
+}
+
+int number = GetInput("Введите число которое нужно " +
+                      "проверить на наличие в массиве");
+int[] arrayTest = CreateArray(10, -10, 10);
+bool thereIsNumberInArray = SearchNumberInArray(arrayTest, number);
+
+Console.Write("[");
+PrintArray(arrayTest);
+Console.WriteLine("]");
+
+Console.WriteLine(thereIsNumberInArray ? $"Число {number} есть в массиве" 
+                                       : $"Число {number} отсутствует в массиве");
