93a88efe8a0d310cc78c1d8539b909e696146c0d
Nikolay470 <dyachkovnick@yandex.ru>
2023-05-28 20:53:08 +0300

    Added a folder Tasks_6_seminar/Task42

---
+++ Tasks_6_seminar/Task42/Program.cs
@@ -0,0 +1,25 @@
+﻿// 42. Напишите программу которая будет преобразовывать десятичное число в двоичное
+
+int GetInput(string message)
+{
+    Console.WriteLine(message);
+    int input = Convert.ToInt32(Console.ReadLine());
+    return input;
+}
+
+string DecToBin(int num)
+{
+    string result = String.Empty;
+    while(num > 0)
+    {
+        result = num % 2 + result;
+        num /= 2;
+    }
+    return result;
+}
+
+
+int number = GetInput("Введите десятичное число "  
+                       + "которое нужно преобразовать в двоичное.");
+string num = DecToBin(number);
+Console.WriteLine($"{num}");
