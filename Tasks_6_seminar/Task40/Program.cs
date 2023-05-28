d16fe1507574306ede80440e97af14f5f5446908
Nikolay470 <dyachkovnick@yandex.ru>
2023-05-28 20:02:41 +0300

    Added a folder Tasks_6_seminar/Task40

---
+++ Tasks_6_seminar/Task40/Program.cs
@@ -0,0 +1,24 @@
+﻿// 40. Напишите программу которая принимает на вход три числа
+//     и проверяет может ли существовать треугольник со сторонами такой длинны. 
+int GetInput(string message)
+{
+    Console.WriteLine(message);
+    int input = Convert.ToInt32(Console.ReadLine());
+    return input;
+}
+
+bool IsTriangle(int side1, int side2, int side3)
+{
+    if (side1 < (side2 + side3) 
+            && side2 < (side1 + side3)
+            && side3 < (side1 + side2)) return true;
+    else return false;
+}
+
+int oneSide = GetInput("Введите первое число");
+int twoSide = GetInput("Введите второе число");
+int threeSide = GetInput("Введите третье число");
+
+bool result = IsTriangle(oneSide, twoSide, threeSide);
+
+Console.WriteLine(result ? "да" : "нет");
