736dafba748e1bb60aa97bf224ac6400c203a69b
Nikolay470 <dyachkovnick@yandex.ru>
2023-06-04 19:12:25 +0300

    Added a folder Tasks_7_seminar/Task51

--- Tasks_7_seminar/Task51/Program.cs
+++ Tasks_7_seminar/Task51/Program.cs
@@ -1,6 +1,5 @@
-﻿// 49. Задайте двумерный массив размером m x n.
-//     Найдите элементы у которых оба индекса четные и
-//     замените эти элементы на их квадраты.
+﻿// 51. Задайте двумерный массив.
+//     Найдите сумму элементов находящихся на главной диагонали.
 
 int[,] CreateMatrixIntRnd(int rows, int columns, int min, int max)
 {
@@ -29,21 +28,18 @@ void PrintMatrix(int[,] arr2d)
     }
 }
 
-void ReplaceElementsOnSquares(int[,] matrix)
+int SumElementsOnDiagonal(int[,] matrix)
 {
-    for (int i = 0; i < matrix.GetLength(0); i += 2)
+    int sum = 0;
+    for (int i = 0; i < matrix.GetLength(0) && i < matrix.GetLength(1); i++)
     {
-        for (int j = 0; j < matrix.GetLength(1); j += 2)
-        {
-            matrix[i, j] *=  matrix[i, j];
-        }
+        sum += matrix[i, i]; 
     }
+    return sum;
 }
 
 int[,] matrixTest = CreateMatrixIntRnd(3, 4, 1, 10);
+int sumElemOnDiagonal = SumElementsOnDiagonal(matrixTest);
 PrintMatrix(matrixTest);
-
 Console.WriteLine();
-
-ReplaceElementsOnSquares(matrixTest);
-PrintMatrix(matrixTest);
+Console.WriteLine($"Сумма элементов находящихся на главной диагонали равна {sumElemOnDiagonal}");
