0c9e9571048daa45816a2a5e0a402e8cbe0b6de5
Nikolay470 <dyachkovnick@yandex.ru>
2023-06-04 18:16:58 +0300

    Added a folder Tasks_7_seminar/Task46

---
+++ Tasks_7_seminar/Task46/Program.cs
@@ -0,0 +1,32 @@
+﻿// 46. Задайте двумерный массив размером m x n,
+//     заполненный случайными целыми числами.
+
+int[,] CreateArray2dIntRnd(int rows, int columns, int min, int max)
+{
+    int[,] arr2d = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < arr2d.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr2d.GetLength(1); j++)
+        {
+            arr2d[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return arr2d;
+}
+
+void PrintArray2d(int[,] arr2d)
+{
+    for (int i = 0; i < arr2d.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < arr2d.GetLength(1); j++)
+        {
+            Console.Write($"{arr2d[i, j],5}");
+        }
+        Console.WriteLine(" |");
+    }
+}
+
+int[,] array2d = CreateArray2dIntRnd(3, 4, -100, 100);
+PrintArray2d(array2d);
