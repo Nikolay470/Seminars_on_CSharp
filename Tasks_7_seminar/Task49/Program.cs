941fd2ce858a4341a1defc4260a673b4b8e3de20
Nikolay470 <dyachkovnick@yandex.ru>
2023-06-04 18:53:50 +0300

    Added a folder Tasks_7_seminar/Task49

---
+++ Tasks_7_seminar/Task49/Program.cs
@@ -0,0 +1,49 @@
+﻿// 49. Задайте двумерный массив размером m x n.
+//     Найдите элементы у которых оба индекса четные и
+//     замените эти элементы на их квадраты.
+
+int[,] CreateMatrixIntRnd(int rows, int columns, int min, int max)
+{
+    int[,] arr2d = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < arr2d.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr2d.GetLength(1); j++)
+        {
+            arr2d[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return arr2d;
+}
+
+void PrintArray2d(int[,] arr2d)
+{
+    for (int i = 0; i < arr2d.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < arr2d.GetLength(1); j++)
+        {
+            Console.Write($"{arr2d[i, j],5}");
+        }
+        Console.WriteLine(" |");
+    }
+}
+
+void ReplaceElementsOnSquares(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i += 2)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j += 2)
+        {
+            matrix[i, j] *=  matrix[i, j];
+        }
+    }
+}
+
+int[,] matrixTest = CreateMatrixIntRnd(3, 4, 1, 10);
+PrintArray2d(matrixTest);
+
+Console.WriteLine();
+
+ReplaceElementsOnSquares(matrixTest);
+PrintArray2d(matrixTest);
