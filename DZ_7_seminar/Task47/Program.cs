3bc4cb9e72a915db6c10d87c9ac26583a4f1a4cc
Nikolay470 <dyachkovnick@yandex.ru>
2023-06-04 20:04:36 +0300

    Added a folder DZ_7_seminar/Task47

---
+++ DZ_7_seminar/Task47/Program.cs
@@ -0,0 +1,45 @@
+﻿// 47. Задайте двумерный массив размером m×n, 
+//     заполненный случайными вещественными числами.
+
+double[,] CreateMatrixDoubleRnd(int rows, int columns, int min, int max)
+{
+    double[,] matrix = new double[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.NextDouble() * (max - min) + min;
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],5}");
+        }
+        Console.WriteLine(" |");
+    }
+}
+
+void RoundElementsInMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Math.Round(matrix[i, j], 1);
+        }
+    }
+}
+
+
+double[,] matrixTest = CreateMatrixDoubleRnd(3, 4, 1, 10);
+RoundElementsInMatrix(matrixTest);
+PrintMatrix(matrixTest);
