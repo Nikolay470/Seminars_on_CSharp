79becba080fe806714c0753f1e428764d4cfdafd
Nikolay470 <dyachkovnick@yandex.ru>
2023-06-09 17:22:48 +0300

    Added a folder Task64

---
+++ DZ/DZ_9_seminar/Task64/Program.cs
@@ -0,0 +1,20 @@
+﻿// 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа 
+//     в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+int GetInput(string message)
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+void NaturalNumbers(int num)
+{
+    if (num == 0) return;
+    Console.Write($"{num} ");
+    NaturalNumbers(num - 1);
+}
+
+int numberN = GetInput("Введите целое положительное число: ");
+if(numberN < 0) Console.WriteLine("число должно быть положительным");
+else NaturalNumbers(numberN);
